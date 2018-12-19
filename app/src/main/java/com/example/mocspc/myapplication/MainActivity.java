f49ed879013dc41aab8c62fb209ecf6235e6a7b1
Moc <superkingblue123@yahoo.co.uk>
2018-12-19 21:10:22 +0100

    put on github

---
+++ app/src/main/java/com/example/mocspc/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mocspc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        test();
+    }
+
+    public void test() {
+        System.out.println("hello");
+    }
+}
